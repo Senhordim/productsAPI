f408dd305caaba47630d071835f9e06e256ed864
Diego Collares <diego.collares@gmail.com>
2017-10-06 18:39:41 -0300

    endpont to update object

--- src/main/java/br/com/productsapi/system/resource/ProductResource.java
+++ src/main/java/br/com/productsapi/system/resource/ProductResource.java
@@ -3,9 +3,11 @@ package br.com.productsapi.system.resource;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,36 +26,34 @@ public class ProductResource {
 	@Autowired
 	private ProductDAO productDAO;
 	
-	/**
-	 * Declrando requisição GET  
-	 * Retornando todos produtos
-	 */
 	@GetMapping
 	public Collection<Product> listAll() {
 		return productDAO.findAll();
 	}
 	
-	/**
-	 * Declrando requisição POST  
-	 * Para inserir um produto
-	 */
 	@PostMapping
 	public Product addProduct(@RequestBody Product product) {
-		return productDAO.save(product);
+		if (product.getId() == null) {
+			return productDAO.save(product);
+		} else {
+			return productDAO.save(product);
+		}
 	}
 	
-
-	
-	/**
-	 * Declrando requisição GET  
-	 * Retorna produto por id
-	 */
 	@GetMapping(value="/{id}")
 	public Product getProductWithId(@PathVariable Long id) {
 		return productDAO.findOne(id);
 	}
 	
+	@DeleteMapping(value="/{id}")
+	public void  deleteProductWithId(@PathVariable Long id) {
+		productDAO.delete(id);
+	}
+	
+	@DeleteMapping
+	public void deleteAllProducts() {
+	    productDAO.deleteAll();
+	}
 	
 	
-
 }
