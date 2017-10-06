25783e34fb93c1ea991605759a6e1246a44e553f
Diego Collares <diego.collares@gmail.com>
2017-10-06 19:53:12 -0300

    remove lines in file

--- src/main/java/br/com/productsapi/system/resource/ProductResource.java
+++ src/main/java/br/com/productsapi/system/resource/ProductResource.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +22,7 @@ public class ProductResource {
 	 * Injeção de depêndencia 
 	 * Inserindo a Interface ProductDAO
 	 */
+	
 	@Autowired
 	private ProductDAO productDAO;
 	
@@ -54,6 +54,5 @@ public class ProductResource {
 	public void deleteAllProducts() {
 	    productDAO.deleteAll();
 	}
-	
 	
 }
